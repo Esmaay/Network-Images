b990b600ff0b8f581b89c71d55cf40574c4698df
Esmaybe <esmaybe@revenity.net>
2024-08-16 15:58:40 +0200

    Remove other extensiosn too, for .old etc

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,8 +23,8 @@ printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 
 if [ -d "/home/container/plugins" ]; then
-    echo "Removing .jar files from /home/container/plugins"
-    find /home/container/plugins -maxdepth 1 -type f -name "*.jar" -exec rm -f {} \;
+    echo "Removing old plugins from /home/container/plugins"
+    find /home/container/plugins -maxdepth 1 ! -type d -exec rm -f {} \;
 fi
 
 wget -r -np -q -nH --cut-dirs=2 --reject index.html,index.html.tmp --accept "*.*" "http://172.18.0.7$GLOBAL_FOLDER/" &
